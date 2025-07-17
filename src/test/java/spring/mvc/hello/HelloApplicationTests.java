fa067e670345a9fb34ebf4002d1ad39805d2201e
brown <brown2243@naver.com>
2025-07-17 16:14:58 +0900

    chore: 프로젝트 세팅

---
+++ src/test/java/spring/mvc/hello/HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.mvc.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
