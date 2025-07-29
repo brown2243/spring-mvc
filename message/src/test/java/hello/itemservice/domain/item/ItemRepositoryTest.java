c653f437c9bd22ecba86bfb80d3bbe62c26fdcbe
brown <brown2243@naver.com>
2025-07-30 03:21:45 +0900

    11-35. 스프링 메시지 소스 설정

--- message/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ message/src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -18,49 +18,49 @@ class ItemRepositoryTest {
 
     @Test
     void save() {
-        //given
+        // given
         Item item = new Item("itemA", 10000, 10);
 
-        //when
+        // when
         Item savedItem = itemRepository.save(item);
 
-        //then
+        // then
         Item findItem = itemRepository.findById(item.getId());
         assertThat(findItem).isEqualTo(savedItem);
     }
 
     @Test
     void findAll() {
-        //given
+        // given
         Item item1 = new Item("item1", 10000, 10);
         Item item2 = new Item("item2", 20000, 20);
 
         itemRepository.save(item1);
         itemRepository.save(item2);
 
-        //when
+        // when
         List<Item> result = itemRepository.findAll();
 
-        //then
+        // then
         assertThat(result.size()).isEqualTo(2);
         assertThat(result).contains(item1, item2);
     }
 
     @Test
     void updateItem() {
-        //given
+        // given
         Item item = new Item("item1", 10000, 10);
 
         Item savedItem = itemRepository.save(item);
         Long itemId = savedItem.getId();
 
-        //when
+        // when
         Item updateParam = new Item("item2", 20000, 30);
         itemRepository.update(itemId, updateParam);
 
         Item findItem = itemRepository.findById(itemId);
 
-        //then
+        // then
         assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
         assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
         assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
