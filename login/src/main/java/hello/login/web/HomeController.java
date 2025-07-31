a1e4bfdedacbc7fe77b3f5f7050e76854a35cda6
brown <brown2243@naver.com>
2025-08-01 01:52:12 +0900

    14-74. 회원 가입

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("")
     public String home() {
-        return "redirect:/items";
+        return "home";
     }
 }
